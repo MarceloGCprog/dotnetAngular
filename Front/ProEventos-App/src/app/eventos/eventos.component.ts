a4f98e05e4b8350b804339524475870085a95006
MarceloGCprog <auditorjamar@gmail.com>
2022-03-18 16:23:09 -0300

    adicao do filtro e novo layout

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos : any = [];
+  public eventosFiltrados: any =[];
+  imagewidth :number = 150;
+  imageheight:number = 100;
+  imagemmargin:number = 2;
+  isCollapsed = true;
+  private _filtrolista : string = '';
 
   constructor(private http: HttpClient) { }
 
@@ -16,13 +22,31 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public get filtrolista():string {
+    return this._filtrolista;
+  }
+
+  public set filtrolista(value:string){
+
+    this._filtrolista = value;
+    this.eventosFiltrados = this.filtrolista ? this.getEventosFiltrados(this.filtrolista) : this.eventos;
+  }
+
   public getEventos(): void{
 
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {this.eventos = response,
+      this.eventosFiltrados = this.eventos},
       error => console.log("Erro:", error)
     );
 
+  }
+
+  public getEventosFiltrados(filtrarPor :string) : any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      ( evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor)!== -1 );
   }
 
 
